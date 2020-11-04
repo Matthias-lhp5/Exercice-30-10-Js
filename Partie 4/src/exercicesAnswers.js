d0ef127bc13556b219bfea4e9c7bd0eefa4a87a7
Matthias-lhp5 <72194766+Matthias-lhp5@users.noreply.github.com>
2020-11-04 13:32:25 +0100

    maj

--- Partie 4/src/exercicesAnswers.js
+++ Partie 4/src/exercicesAnswers.js
@@ -1,41 +1,48 @@
 //Exercice 1 : Créer un tableau days et lui ajouter les jours de la semaine.
 let createArray = function () {
+  let days = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
   return days;
 }
 
 //Exercice 2 : Retouner le 3ème élément du tableau days', function() {
 let returnThirdElement = function (array) {
-  return 'A compléter';
+  return array[2];
 }
 
 //Exercice 3 : Afficher la valeur de l\'index 3 du tableau days.', function() {
 let returnThirdIndex = function (array) {
-  return 'A compléter';
+  return array[3];
 }
 
 //Exercice 4 : Changer le 2ème élément du tableau languages par php.', function() {
 let changeSecondElement = function (array) {
-  return 'A compléter';
+ // array[1] = 'Php'
+  array.splice(1, 1, 'php');
+  return array;
 }
 
 //Exercice 5 : Ajouter ruby et python à la fin du tableau languages.', function() {
 let addAtTheEnd = function (array) {
-  return 'A compléter';
+  array.push('ruby', 'python')
+  return array;
 }
 
 //Exercice 6 : Ajouter html et css au début du tableau languages.', function() {
 let addAtFirst = function (array) {
-  return 'A compléter';
+  array.unshift('html', 'css')
+  return array;
 }
 
 //Exercice 7 : Supprimer le premier élément du tableau languages.', function() {
 let removeFirstElement = function (array) {
-  return 'A compléter';
+  array.shift()
+  return array;
 }
 
 //Exercice 8 : Supprimer le dernier élément du tableau languages.', function() {
 let removeLastElement = function (array) {
-  return 'A compléter';
+  array.pop()
+  return array;
 }
 
 //Exercice 9 : Créer un tableau associatif person. Lui ajouter les index name, age et city avec les valeurs Jean, 45, Paris.', function() {
